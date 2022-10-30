4725ae44863db2cbbadd602b4af118d1aec9d9c4
Mohammed Sohail Uddin <70440734+MohammedSohailUddin@users.noreply.github.com>
2022-10-30 17:32:31 +0530

    added targets

--- src/Exercise1/JsonIgnore.java
+++ src/Exercise1/JsonIgnore.java
@@ -3,12 +3,15 @@ package Exercise1;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
 
 //User-defined annotation
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD}) 
 @interface JsonIgnore
 {
-	String Name() default "Sohail";
+	String Name();
 	String DoB();
 } 
